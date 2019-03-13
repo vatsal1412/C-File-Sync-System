23e70899db08206249f6b1d4358dae503ee1282f
vatsal1412 <vatsal1412@gmail.com>
2019-03-13 16:32:08 -0400

    Added functionality to organize files into a tree structure

--- ftree.c
+++ ftree.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-// Add your system includes here.
+//system includes
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
--- ftree.h
+++ ftree.h
@@ -6,6 +6,7 @@
  * is the number of processes involved in the copy and is
  * at least 1.
  */
+ 
 int copy_ftree(const char *src, const char *dest);
 
 #endif // _FTREE_H_
